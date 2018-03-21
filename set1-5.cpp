948df77cd783303860d116f530222eb59f025cec
Srivatsan14 <36353051+Srivatsan14@users.noreply.github.com>
2018-03-21 12:12:25 +0530

    Create set1-5.cpp

---
+++ set1-5.cpp
@@ -0,0 +1,21 @@
+#include<iostrem.h>
+#include<conio.h>
+void main()
+{
+clrscr()
+{
+int a,b,c;
+if((a>b) && (a>c))
+{
+cout<<"a is greater ";
+}
+else if (b>c)
+{
+cout<<"b is greater";
+}
+else
+{
+cout<<"c is greater";
+}
+}
+}
