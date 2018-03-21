f325d1ac65f3b2b6901fdba91c42a59c9152ebc3
Srivatsan14 <36353051+Srivatsan14@users.noreply.github.com>
2018-03-21 11:58:01 +0530

    Create set1-3.cpp

---
+++ set1-3.cpp
@@ -0,0 +1,16 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+
+char s;
+cin>>s;
+if (s=='a' || s=='e' || s=='i' || s=='o' || s=='u')
+{
+cout<<"vowel";
+}
+else
+{
+cout<<"consonant";
+}
+}
